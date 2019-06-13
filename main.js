702abdf3c83e050e24e0753da83945055cf3c85e
Aimal Sayeq <aimalsayeq@gmail.com>
2019-06-12 20:54:26 -0400

    updated

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 var name = "This is Variable"
+var number = 4 + 4 * (2 - 3) ;
+// alert("Hello, " + name);
+// alert("Hello, " + number);
 
-alert("Hello " + name);
+var x = 'test';
+
+var y = 'this is a test';
+
+var z = 'that is not what you think';
+
+// alert('length of x ' + x.length);
+// alert('length of y ' + y.length);
+//
+// alert('length of z ' + z.length);
+
+function alertLength(item) {
+  alert('length of ' + String(item) + ' ' + item.length)
+}
+
+var items = [x, y, z];
+
+items.forEach(function(item) {
+  alertLength(item);
+});
+
+
 
 //alert("This is JavaScript");
