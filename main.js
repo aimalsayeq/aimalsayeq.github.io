facc4403e874c9e0877dc69cd9674b60c4e23033
Aimal Sayeq <aimalsayeq@gmail.com>
2019-06-12 20:56:04 -0400

    updated

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-var name = "This is Variable"
-var number = 4 + 4 * (2 - 3) ;
+//var name = "This is Variable"
+//var number = 4 + 4 * (2 - 3) ;
 // alert("Hello, " + name);
 // alert("Hello, " + number);
 
-var x = 'test';
+//var x = 'test';
 
-var y = 'this is a test';
+//var y = 'this is a test';
 
-var z = 'that is not what you think';
+//var z = 'that is not what you think';
 
 // alert('length of x ' + x.length);
 // alert('length of y ' + y.length);
 //
 // alert('length of z ' + z.length);
 
-function alertLength(item) {
-  alert('length of ' + String(item) + ' ' + item.length)
-}
+//function alertLength(item) {
+  //alert('length of ' + String(item) + ' ' + item.length)
+//}
 
-var items = [x, y, z];
+//var items = [x, y, z];
 
-items.forEach(function(item) {
-  alertLength(item);
-});
+//items.forEach(function(item) {
+//  alertLength(item);
+//});
 
 
 
